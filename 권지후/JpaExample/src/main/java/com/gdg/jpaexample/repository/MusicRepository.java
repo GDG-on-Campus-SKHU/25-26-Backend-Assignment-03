4d97a41bfb16270ac47d60cdd327a9bc88d2bca5
leebohyeon <lilddy35@gmail.com>
2025-11-03 00:53:04 +0900

    one

--- 권지후/JpaExample/src/main/java/com/gdg/jpaexample/repository/MusicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gdg.jpaexample.repository;
-
-import com.gdg.jpaexample.domain.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicRepository extends JpaRepository<Music, Long> {
-}
