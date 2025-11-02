1ab5cb0dd86fef205aebd504b47dc2e028554fd1
seokhawn01 <skhu202214139@gmail.com>
2025-11-03 00:41:04 +0900

    리펙토링

--- 25-26-Backend-Assignment-03/JpaWork/src/main/java/com/example/jpawork/domain/Exercise_area.java
+++ 25-26-Backend-Assignment-03/JpaWork/src/main/java/com/example/jpawork/domain/Exercise_area.java
@@ -1,6 +1,7 @@
 package com.example.jpawork.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import java.util.List;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Exercise_area {
 
     @Id
