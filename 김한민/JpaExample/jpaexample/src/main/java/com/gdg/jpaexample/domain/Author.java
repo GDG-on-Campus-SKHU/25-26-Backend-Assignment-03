6317a5f8c6287c360488f961e68731a5828774eb
Kim, Hanmin <kimhanmin0601@gmail.com>
2025-11-05 06:37:54 +0900

    refactor: @Builder.Default 제거 및 코드 정리

--- 김한민/JpaExample/jpaexample/src/main/java/com/gdg/jpaexample/domain/Author.java
+++ 김한민/JpaExample/jpaexample/src/main/java/com/gdg/jpaexample/domain/Author.java
@@ -1,34 +1,49 @@
 package com.gdg.jpaexample.domain;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "author")
 public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false, length = 50)
     private String nationality;
 
-    @Builder.Default
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Book> books = new ArrayList<>();
+    private List<Book> books;
+
+    @Builder
+    public Author(String name, String nationality, List<Book> books) {
+        this.name = name;
+        this.nationality = nationality;
+        // 빌더로 생성 시 books를 세팅하지 않으면 null이 될 수 있으니 기본값 보장
+        this.books = (books != null) ? books : new ArrayList<>();
+    }
+
+    // 양방향 편의 메서드(선택)
+    public void addBook(Book book) {
+        this.books.add(book);
+        book.setAuthor(this);
+    }
 }
--- 김한민/JpaExample/jpaexample/src/main/java/com/gdg/jpaexample/domain/Book.java
+++ 김한민/JpaExample/jpaexample/src/main/java/com/gdg/jpaexample/domain/Book.java
@@ -1,5 +1,6 @@
 package com.gdg.jpaexample.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -7,33 +8,46 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor
+@Entity
+@Table(name = "book")
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 150)
     private String title;
+
+    @Column(name = "published_year", nullable = false)
     private int publishedYear;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
+    @JoinColumn(name = "author_id", nullable = false)
     private Author author;
+
+    @Builder
+    public Book(String title, int publishedYear, Author author) {
+        this.title = title;
+        this.publishedYear = publishedYear;
+        this.author = author;
+    }
 
     public void update(String title, int publishedYear, Author author) {
         this.title = title;
         this.publishedYear = publishedYear;
+        this.author = author;
+    }
+
+    // Author.addBook에서 호출
+    void setAuthor(Author author) {
         this.author = author;
     }
 }
